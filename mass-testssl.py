45e23903a82503919ff73e64f4e12ea9ea181d32
cbk914 <cbk914@riseup.net>
2023-02-15 19:58:29 +0100

    Update mass-testssl.py

--- mass-testssl.py
+++ mass-testssl.py
@@ -1,55 +1,72 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Author: cbk914
+#!/usr/bin/env python3
+
 import subprocess
 import argparse
 import os
 import socket
 
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Scan for SSL/TLS security vulnerabilities using testssl.sh')
+parser.add_argument('-f', '--file', required=True, help='input file with IP addresses or URLs')
+parser.add_argument('-o', '--output', help='output directory for HTML reports')
+args = parser.parse_args()
 
-def parse_ips(ips_file):
-    with open(ips_file, 'r') as f:
-        ips = f.read()
-    ips = ips.replace('\n', ',')
-    ips = ips.replace(' ', ',')
-    ips = ips.strip(',')
-    ips = ips.split(',')
-    ips = list(set(ips))
-    return ips
+# Find testssl.sh on the system
+testssl_path = subprocess.check_output(['which', 'testssl.sh']).strip().decode('utf-8')
 
+# Initialize output directory
+if args.output:
+    output_dir = args.output
+else:
+    output_dir = os.getcwd()
 
-def run_testssl(ip, testssl_path, output_path):
-    print(f"Scanning {ip}...")
-    cmd = f"{testssl_path} -9 --html {ip} > {output_path}/{ip}.html"
-    subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print(f"Finished scanning {ip}.")
+# Process input file
+ips = set()
+with open(args.file, 'r') as f:
+    for line in f:
+        for ip in line.strip().split(','):
+            ips.add(ip.strip())
 
+# Initialize results report
+results = {}
 
-def main():
-    parser = argparse.ArgumentParser(description='Scan IPs with testssl.sh')
-    parser.add_argument('-f', '--file', dest='ips_file', required=True, help='Text file containing list of IPs to scan')
-    parser.add_argument('-t', '--testssl', dest='testssl_path', default='testssl.sh', help='Path to testssl.sh')
-    parser.add_argument('-o', '--output', dest='output_path', default='output', help='Output directory for HTML results')
-    args = parser.parse_args()
+# Scan each IP with testssl
+for i, ip in enumerate(ips):
+    print(f"Scanning IP {ip} ({i+1}/{len(ips)})...")
+    # Check if IP is valid
+    try:
+        socket.inet_aton(ip)
+    except socket.error:
+        print(f"Error: {ip} is not a valid IP address")
+        results[ip] = "Invalid IP address"
+        continue
+    # Run testssl on IP
+    output_file = os.path.join(output_dir, f"testssl_{ip.replace('.', '_')}.html")
+    if os.path.isfile(output_file):
+        print(f"Skipping IP {ip}: already scanned")
+        results[ip] = "Already scanned"
+        continue
+    testssl_cmd = f"{testssl_path} -9 --html {ip} > {output_file}"
+    process = subprocess.Popen(testssl_cmd, stdout=subprocess.PIPE, shell=True, universal_newlines=True)
+    output = ""
+    while True:
+        line = process.stdout.readline()
+        if not line:
+            break
+        print(line, end='')
+        output += line
+    process.wait()
+    # Check for errors
+    if process.returncode != 0:
+        print(f"Error: testssl returned non-zero exit code ({process.returncode}) for IP {ip}")
+        results[ip] = "Scan failed"
+        continue
+    results[ip] = "Scan successful"
 
-    if not os.path.exists(args.output_path):
-        os.makedirs(args.output_path)
-
-    ips = parse_ips(args.ips_file)
-
-    results = []
-    for ip in ips:
-        try:
-            run_testssl(ip, args.testssl_path, args.output_path)
-            results.append((ip, 'SUCCESS'))
-        except Exception as e:
-            print(f"Failed to scan {ip}. Error message: {str(e)}")
-            results.append((ip, 'FAILED'))
-
-    print("\n=== Scan Results ===")
-    for ip, result in results:
-        print(f"{ip}: {result}")
-
-
-if __name__ == '__main__':
-    main()
+# Print results report
+print("\nScan results:")
+for ip, result in results.items():
+    print(f"{ip}: {result}")
