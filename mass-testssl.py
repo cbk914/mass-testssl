8ea85612c91cefd99374a32c754599a83992811d
cbk914 <cbk914@riseup.net>
2023-02-15 16:25:05 +0100

    Update mass-testssl.py

--- mass-testssl.py
+++ mass-testssl.py
@@ -6,14 +6,18 @@ import argparse
 import os
 import socket
 
-# Define a function to run testssl.sh for each IP address, range, and URL in the provided list file
 def run_testssl(ip_list_file):
+    # Check if the input file exists and is a file
+    if not os.path.isfile(ip_list_file):
+        print(f"Input file '{ip_list_file}' does not exist or is not a file")
+        return
+
     # Read in the list of IP addresses, ranges, and URLs from the provided file
     with open(ip_list_file, 'r') as f:
         ip_list = [line.strip() for line in f]
 
-    # Filter out any empty lines
-    ip_list = filter(None, ip_list)
+    # Filter out any empty or commented lines
+    ip_list = [line for line in ip_list if line and not line.startswith('#')]
 
     # Create a subdirectory for the testssl.sh output files
     output_dir = 'testssl_output'
@@ -25,41 +29,40 @@ def run_testssl(ip_list_file):
     # Run testssl.sh for each IP address, range, and URL in the list
     for index, ip in enumerate(ip_list):
         # Split the line on commas to handle CSV format
-        ips = ip.split(',')
+        ips = [i.strip() for i in ip.split(',')]
 
-        # Remove any leading or trailing whitespace from each IP address or URL
-        ips = [i.strip() for i in ips]
-
+        # Validate each IP address or URL in the list
         for i in ips:
-            # Validate the IP address or URL
-            if i == '':
+            # Skip empty or invalid IP addresses or URLs
+            try:
+                socket.getaddrinfo(i, None, socket.AF_INET)
+            except socket.gaierror:
+                print(f"Invalid IP address or URL: {i}")
                 continue
-            elif socket.getaddrinfo(i, None, socket.AF_INET):
-                command = ['testssl.sh', '-9', '--html', i]
-            else:
-                print(f'Invalid IP address or URL: {i}')
-                continue
+
+            # Build the testssl.sh command for the current IP address or URL
+            command = ['testssl.sh', '-9', '--html', i]
 
             # Run testssl.sh for the current IP address or URL
-            print(f'Running testssl.sh for {i}... ({round((index + 1) / total_ips * 100, 2)}%)')
+            print(f"Running testssl.sh for {i}... ({round((index + 1) / total_ips * 100, 2)}%)")
             try:
                 result = subprocess.run(command, check=True, capture_output=True, text=True, cwd=output_dir)
-                if result.returncode == 0:
-                    print(f'Successfully ran testssl.sh for {i}')
-                else:
-                    print(f'Error running testssl.sh for {i}: {result.stderr}')
-            except subprocess.CalledProcessError as e:
-                print(f'Error running testssl.sh for {i}: {e.stderr}')
-            except Exception as e:
-                print(f'Unknown error running testssl.sh for {i}: {e}')
 
-# Define a function to parse command-line arguments
+                if result.returncode == 0:
+                    print(f"Successfully ran testssl.sh for {i}")
+                else:
+                    print(f"Error running testssl.sh for {i}: {result.stderr}")
+            except subprocess.CalledProcessError as e:
+                print(f"Error running testssl.sh for {i}: {e.stderr}")
+            except Exception as e:
+                print(f"Unknown error running testssl.sh for {i}: {e}")
+
 def parse_args():
-    parser = argparse.ArgumentParser(description='Run testssl.sh for a list of IP addresses, ranges, and URLs')
-    parser.add_argument('-f', '--file', type=str, required=True, help='Path to file containing list of IP addresses, ranges, and URLs')
+    parser = argparse.ArgumentParser(description="Run testssl.sh for a list of IP addresses, ranges, and URLs")
+    parser.add_argument("-f", "--file", type=str, required=True, help="Path to file containing list of IP addresses, ranges, and URLs")
     return parser.parse_args()
 
-# Run the script
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     run_testssl(args.file)
+
