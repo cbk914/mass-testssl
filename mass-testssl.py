c439435730554988b2162e71a9a96d8cd1f27a7c
cbk914 <cbk914@riseup.net>
2023-02-15 19:38:15 +0100

    Update mass-testssl.py

--- mass-testssl.py
+++ mass-testssl.py
@@ -5,101 +5,82 @@ import subprocess
 import argparse
 import os
 import socket
+import sys
 
-# Function to check if an IP address is valid
-def is_valid_ip(ip):
+def main():
+    parser = argparse.ArgumentParser(description='testssl.sh script.')
+    parser.add_argument('-f', '--file', help='File with the list of IP, Ranges or URLs.')
+    parser.add_argument('-o', '--output', default='output', help='Output directory for testssl.sh scan results')
+    args = parser.parse_args()
+
+    if not args.file:
+        print('Please provide a file with the -f option.')
+        sys.exit(1)
+
+    with open(args.file) as f:
+        content = f.read()
+
+    # Filter out empty lines and duplicates
+    ips = set(filter(None, (ip.strip() for ip in content.split(','))))
+
+    # Get testssl.sh path
     try:
-        socket.inet_pton(socket.AF_INET, ip)
-        return True
-    except socket.error:
-        return False
+        testssl_path = subprocess.check_output(['which', 'testssl.sh']).decode().strip()
+    except subprocess.CalledProcessError:
+        print('Unable to find testssl.sh on the system.')
+        sys.exit(1)
 
-# Function to check if a URL is valid
-def is_valid_url(url):
-    try:
-        result = urlparse(url)
-        return all([result.scheme, result.netloc])
-    except ValueError:
-        return False
-
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Scan IPs and URLs with testssl.sh')
-parser.add_argument('-f', '--file', help='Input file containing IPs and URLs')
-args = parser.parse_args()
-
-# Check if input file was provided
-if not args.file:
-    parser.error('Please provide an input file using the -f option')
-
-# Get the absolute path of the testssl.sh script
-testssl_path = subprocess.check_output('which testssl.sh', shell=True, encoding='utf-8').strip()
-
-# Read input file
-with open(args.file, 'r') as f:
-    lines = f.readlines()
-
-# Initialize list to keep track of scanned IPs and URLs
-scanned = []
-
-# Loop through lines in the input file
-for i, line in enumerate(lines):
-    # Strip whitespace from the line
-    line = line.strip()
-    
-    # Split line into individual IPs and URLs
-    addrs = [addr.strip() for addr in line.split(',')]
-
-    # Loop through IPs and URLs in the line
-    for addr in addrs:
-        # Check if the IP or URL has already been scanned
-        if addr in scanned:
-            continue
-        
-        # Add the IP or URL to the list of scanned addresses
-        scanned.append(addr)
-
-        # Check if the address is a valid IP
-        if is_valid_ip(addr):
-            print(f'Scanning IP address: {addr}')
-        # Check if the address is a valid URL
-        elif is_valid_url(addr):
-            print(f'Scanning URL: {addr}')
-        else:
-            print(f'Invalid address: {addr}')
+    progress = 0
+    total = len(ips)
+    for ip in ips:
+        try:
+            socket.inet_aton(ip)
+        except socket.error:
+            print(f'Invalid IP address or hostname: {ip}')
             continue
 
-        # Run testssl.sh on the address
-        command = f'{testssl_path} -9 --html {addr}'
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='latin-1')
+        # Run testssl.sh on the IP
+        command = f'{testssl_path} -9 --html {ip}'
+        try:
+            proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        except OSError:
+            print(f'Unable to execute command: {command}')
+            continue
 
-        # Initialize variables to keep track of progress
-        total_lines = 0
-        lines_read = 0
+        # Print output while the process is running
+        while proc.poll() is None:
+            output = proc.stdout.readline().decode('utf-8', errors='replace')
+            if output:
+                if 'testssl.sh' in output:
+                    # Ignore testssl.sh version output
+                    continue
+                elif ' 100.00%' in output:
+                    # Ignore final output lines
+                    continue
+                else:
+                    print(output.strip())
 
-        # Loop through the output of testssl.sh
-        for line in iter(process.stdout.readline, ''):
-            # Count the total number of lines in the output
-            if not total_lines:
-                total_lines = len(line)
-                print(f'Total lines: {total_lines}')
-            
-            # Replace non-ASCII characters with a placeholder
-            line = ''.join([c if ord(c) < 128 else '?' for c in line])
-            
-            # Print the line
-            print(line.strip())
+                    # Update progress
+                    progress += 1
+                    percent_complete = round(progress / total * 100, 2)
+                    print(f'Progress: {percent_complete}%')
 
-            # Increment the number of lines read
-            lines_read += 1
+        # Print any remaining output
+        for output in proc.stdout.readlines():
+            if output:
+                if 'testssl.sh' in output:
+                    # Ignore testssl.sh version output
+                    continue
+                elif ' 100.00%' in output:
+                    # Ignore final output lines
+                    continue
+                else:
+                    print(output.decode('utf-8', errors='replace').strip())
 
-            # Print the progress
-            progress = int((lines_read/total_lines)*100)
-            print(f'Progress: {progress}%')
+                    # Update progress
+                    progress += 1
+                    percent_complete = round(progress / total * 100, 2)
+                    print(f'Progress: {percent_complete}%')
 
-        # Print any errors
-        for line in iter(process.stderr.readline, ''):
-            print(line.strip())
-
-        # Wait for the process to finish
-        process.wait()
-
+if __name__ == '__main__':
+    main()
