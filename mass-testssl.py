de2f019c7379d2853f0ff48098fba9c37f05a1af
cbk914 <cbk914@riseup.net>
2023-02-15 19:33:32 +0100

    Update mass-testssl.py

--- mass-testssl.py
+++ mass-testssl.py
@@ -5,74 +5,101 @@ import subprocess
 import argparse
 import os
 import socket
-import ipaddress
-from tqdm import tqdm
 
-
-def parse_args():
-    parser = argparse.ArgumentParser(description='Automate testssl.sh scans on multiple IP addresses')
-    parser.add_argument('-f', '--file', required=True, help='Text file containing a list of IP addresses and URLs')
-    parser.add_argument('-o', '--output', default='output', help='Output directory for testssl.sh scan results')
-    return parser.parse_args()
-
-
-def find_testssl_sh():
+# Function to check if an IP address is valid
+def is_valid_ip(ip):
     try:
-        path = subprocess.check_output(['which', 'testssl.sh'], universal_newlines=True).strip()
-    except subprocess.CalledProcessError:
-        return None
-    else:
-        return path
+        socket.inet_pton(socket.AF_INET, ip)
+        return True
+    except socket.error:
+        return False
 
+# Function to check if a URL is valid
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
 
-def scan_ips(ip_list, output_dir, testssl_path):
-    unique_ips = set(ip_list)  # create a set of unique IP addresses
-    for ip in tqdm(unique_ips, desc='Scanning', unit='IP'):
-        # validate the IP address
-        try:
-            ipaddress.ip_address(ip)
-        except ValueError:
-            print(f'Invalid IP address: {ip}')
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Scan IPs and URLs with testssl.sh')
+parser.add_argument('-f', '--file', help='Input file containing IPs and URLs')
+args = parser.parse_args()
+
+# Check if input file was provided
+if not args.file:
+    parser.error('Please provide an input file using the -f option')
+
+# Get the absolute path of the testssl.sh script
+testssl_path = subprocess.check_output('which testssl.sh', shell=True, encoding='utf-8').strip()
+
+# Read input file
+with open(args.file, 'r') as f:
+    lines = f.readlines()
+
+# Initialize list to keep track of scanned IPs and URLs
+scanned = []
+
+# Loop through lines in the input file
+for i, line in enumerate(lines):
+    # Strip whitespace from the line
+    line = line.strip()
+    
+    # Split line into individual IPs and URLs
+    addrs = [addr.strip() for addr in line.split(',')]
+
+    # Loop through IPs and URLs in the line
+    for addr in addrs:
+        # Check if the IP or URL has already been scanned
+        if addr in scanned:
+            continue
+        
+        # Add the IP or URL to the list of scanned addresses
+        scanned.append(addr)
+
+        # Check if the address is a valid IP
+        if is_valid_ip(addr):
+            print(f'Scanning IP address: {addr}')
+        # Check if the address is a valid URL
+        elif is_valid_url(addr):
+            print(f'Scanning URL: {addr}')
+        else:
+            print(f'Invalid address: {addr}')
             continue
 
-        # execute testssl.sh with the IP address and output directory
-        output_file = os.path.join(output_dir, f'{ip}.html')
-        cmd = f'{testssl_path} -9 --html --file {output_file} {ip}'
-        try:
-            subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        except subprocess.CalledProcessError as e:
-            print(f'Error scanning IP {ip}: {e.stderr}')
+        # Run testssl.sh on the address
+        command = f'{testssl_path} -9 --html {addr}'
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='latin-1')
 
+        # Initialize variables to keep track of progress
+        total_lines = 0
+        lines_read = 0
 
-def main():
-    args = parse_args()
+        # Loop through the output of testssl.sh
+        for line in iter(process.stdout.readline, ''):
+            # Count the total number of lines in the output
+            if not total_lines:
+                total_lines = len(line)
+                print(f'Total lines: {total_lines}')
+            
+            # Replace non-ASCII characters with a placeholder
+            line = ''.join([c if ord(c) < 128 else '?' for c in line])
+            
+            # Print the line
+            print(line.strip())
 
-    # create the output directory if it doesn't exist
-    os.makedirs(args.output, exist_ok=True)
+            # Increment the number of lines read
+            lines_read += 1
 
-    # find the path to the testssl.sh script
-    testssl_path = find_testssl_sh()
-    if not testssl_path:
-        print('testssl.sh not found on the system')
-        return
+            # Print the progress
+            progress = int((lines_read/total_lines)*100)
+            print(f'Progress: {progress}%')
 
-    # read the list of IP addresses and URLs from the input file
-    with open(args.file, 'r') as f:
-        ip_list = []
-        for line in f:
-            line = line.strip()
-            if line:
-                for delimiter in [',', ';', ' ']:  # add any other delimiters you want to support
-                    if delimiter in line:
-                        parts = line.split(delimiter)
-                        ip_list.extend(parts)
-                        break
-                else:
-                    ip_list.append(line)
+        # Print any errors
+        for line in iter(process.stderr.readline, ''):
+            print(line.strip())
 
-    # scan the IP addresses using testssl.sh
-    scan_ips(ip_list, args.output, testssl_path)
+        # Wait for the process to finish
+        process.wait()
 
-
-if __name__ == '__main__':
-    main()
